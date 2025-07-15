a99b2d3a78cedf31adfadb2747983eae4ac4cefa
NathzDevs <nathzliban@gmail.com>
2025-07-16 05:42:37 +0800

    Update active-session.js

--- script/active-session.js
+++ script/active-session.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const pogi = "61578026700230";
+  const pogi = "61578254222670";
    if (!pogi.includes(event.senderID))
    return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
   const { threadID, messageID } = event;
