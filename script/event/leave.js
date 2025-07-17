c9ea26190ce576c9a0570914ea143806280a1d8e
NathzDevs <nathzliban@gmail.com>
2025-07-17 15:05:24 +0800

    Update leave.js

--- script/event/leave.js
+++ script/event/leave.js
@@ -1,24 +1,43 @@
-﻿module.exports.config = {
-	name: "leave",
-	eventType: ["log:unsubscribe"],
-	version: "1.0.0",
-	credits: "ryuko",
-	description: "notify leave.",
+const axios = require('axios');
+const fs = require('fs');
+
+module.exports.config = {
+    name: "leave",
+    version: "1.0.0",
 };
 
-module.exports.run = async function({ api, event, Users, Threads }) {
-	try {
-        if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
-	const { threadID } = event;
-	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
-	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "ingat sa byahe haha" : "ayan mateluk ka kase haha";
-	var msg, formPush
-	(typeof data.customLeave == "undefined") ? msg = "fly high {name}, {type}" : msg = data.customLeave;
-	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+module.exports.handleEvent = async function ({ api, event }) {
+    if (event.logMessageType === "log:unsubscribe") {
+        const leftID = event.logMessageData.leftParticipantFbId;
+        let name = await api.getUserInfo(leftID).then(info => info[leftID].name);
 
-	var formPush = { body: msg }
-	
-	return api.sendMessage(formPush, threadID);
-    } catch (err) {}
-}
+        // Truncate name if it's too long
+        const maxLength = 15;
+        if (name.length > maxLength) {
+            name = name.substring(0, maxLength - 3) + '...';
+        }
+
+        const groupInfo = await api.getThreadInfo(event.threadID);
+        const groupName = groupInfo.threadName || "this group";
+        const memberCount = groupInfo.participantIDs.length;
+        const background = groupInfo.imageSrc || "https://i.ibb.co/4YBNyvP/images-76.jpg";
+
+        const url = `https://mademoiselle-rrest-apis-rr28.onrender.com/api/goodbye?pp=https://api-canvass.vercel.app/profile?uid=${leftID}&nama=${encodeURIComponent(name)}&bg=${encodeURIComponent(background)}&member=${memberCount}`;
+
+        try {
+            const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+            const filePath = './script/cache/goodbye_image.jpg';
+            fs.writeFileSync(filePath, Buffer.from(data));
+
+            api.sendMessage({
+                body: `👋 ${name} has left ${groupName}. Inagat ka tanga kapa naman!`,
+                attachment: fs.createReadStream(filePath)
+            }, event.threadID, () => fs.unlinkSync(filePath));
+        } catch (error) {
+            console.error("Error fetching goodbye image:", error);
+            api.sendMessage({
+                body: `👋 ${name} has left ${groupName}.`
+            }, event.threadID);
+        }
+    }
+};
